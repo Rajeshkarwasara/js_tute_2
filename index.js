720e620c9869eb7c191401615cad9fefad6c87e8
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 10:07:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+function hello(s){
+    console.log("hello",s, "how are you")
+}
+hello("rajesh");
+hello("rajpal");
+
+
+
+// next
+function sum1(a1,b1){
+    let r1=a1+b1;
+    console.log(r1)
+}
+sum1(2,2)
+
+
+
+
+// next
+function sum2(){
+    let a=2;
+    let b=3;
+    let c=a+b;
+    return c;
+}
+let d=sum2();
+console.log(d);
+
+
+
+let a=3;
+let b=2;
+console.log("multiply", a*b);
+
+
+
+// next
+let a2=3;
+let b2=2;
+a2++;
+
+ a2 %= 3;
+let x1=a2+b2;
+console.log(x1);
+console.log(typeof(x1))
+
+
+
+// next
+// alert("hello")
+let age4=61;
+if(age4>=18 && age4<=60){
+    console.log("yes")
+}
+else{
+    console.log("not")
+}
+
+
+// next
+function multiply(a5,b5){
+    console.log("multiply",a5*b5);
+}
+multiply(2,2);
+
+
+// next
+function addctio(a6,b6){
+    console.log("addctio",a6+b6);
+}
+addctio(3,2);
+
+
+
+// next
+function sum7(){
+    let a7=2;
+    let b7=4;
+    let c7=a7+b7;
+    return c7;
+}
+let d7=sum7();
+console.log(d7);
+
+
+// next
+function grating(b8){
+    console.log("hello",b8, "how are you")
+}
+grating("rajesh");
+grating("naven");
+
+
+
+// next
+let age =18;
+if(age==18){
+    console.log("yes")
+}
+else{
+    console.log("not")
+}
+
+
+
+// next
+function multiply(a,b){
+    console.log("multiply",a*b)
+}
+multiply(2,3);
+
+
+
+// next
+function add(a,b){
+    console.log("add",a+b)
+}
+add(2,3);
+add(3,3);
+add(4,6);
+multiply(2,4);
+
+
+
+
+// next
+function name(b){
+    console.log("hello",b,"how are you")
+}
+name("naresh");
+name("rahul");
+name("rajesh");
